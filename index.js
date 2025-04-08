1f730c9811b27b758826571636501178205621d0
Sahil Patil <sahilpatil221004@gmai.com>
2025-04-08 21:44:22 +0530

    Fixed error

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ app.use('/api/portfolio', portfolioRoutes);
 
 app.use(errorHandler);
 
-const cors = require('cors');
-
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
