125b1bfe0d1ec894c7814cf68d11c5418cc33e62
Sahil Patil <sahilpatil221004@gmai.com>
2025-04-08 21:40:11 +0530

    Fix Error

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const { errorHandler } = require('./middleware/error');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://portfolio-frontend-dun-mu.vercel.app/',
+  credentials: true,
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/portfolio', portfolioRoutes);
@@ -17,11 +20,6 @@ app.use('/api/portfolio', portfolioRoutes);
 app.use(errorHandler);
 
 const cors = require('cors');
-app.use(cors({
-  origin: 'https://portfolio-frontend-dun-mu.vercel.app/',
-  credentials: true,
-}));
-
 
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
